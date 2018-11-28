785c612f08cff044c4884df135a82c2d41771721
Eric Bailey <e.bailey@sportradar.com>
2018-11-28 02:08:05 -0600

    [ch02] Matching needles

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 fn main() {
-    let needle = 42;
     let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
 
-    for candidate in haystack.iter() {
-        if candidate == &needle {
-            println!("{}", candidate);
-            break
-        } else {
-            print!(".");
+    for reference in haystack.iter() {
+        let item = *reference;
+        let result = match item {
+            42 | 132 => "hit!",
+            _        => "miss"
+        };
+
+        if result == "hit!" {
+            println!("{}: {}", item, result);
         }
     }
 }
