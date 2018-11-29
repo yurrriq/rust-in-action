bbeb66319bf2b8516ea48246a986b76033d3472f
Eric Bailey <e.bailey@sportradar.com>
2018-11-29 02:20:34 -0600

    [ch02] ch2-needle-in-haystack.rs: init

---
+++ src/bin/ch2-needle-in-haystack.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let needle = 42;
+    let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
+
+    for candidate in haystack.iter() {
+        if candidate == &needle {
+            println!("{}", candidate);
+            break
+        } else {
+            print!(".");
+        }
+    }
+}
