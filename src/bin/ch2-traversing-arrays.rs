9bc1324db94f846c2850b3916ccce4a1b7830eb3
Eric Bailey <e.bailey@sportradar.com>
2018-11-29 01:34:48 -0600

    [ch02] Add array traversal example

---
+++ src/bin/ch2-traversing-arrays.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let one             = [1, 2, 3];
+    let two: [u8; 3]    = [1, 2, 3];
+    let blank1          = [0; 3];
+    let blank2: [u8; 3] = [0; 3];
+
+    let arrays = [one, two, blank1, blank2];
+
+    for array in &arrays {
+        print!("{:?}: ", array);
+        for num in array.iter() {
+            print!("\t{} + 10 = {}", num, num + 10);
+        }
+
+        let mut sum = 0;
+        for idx in 0..array.len() {
+            sum += array[idx]; // bounds checked
+        }
+        print!("\t(Σ{:?} = {})", array, sum);
+        println!("");
+    }
+}
