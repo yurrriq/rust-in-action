befcdeb2748f7433a169a8cc4ed45669af24a6a8
Eric Bailey <e.bailey@sportradar.com>
2018-11-29 02:21:18 -0600

    [ch02] ch2-match-needles.rs: init

---
+++ src/bin/ch2-match-needles.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
+
+    for reference in haystack.iter() {
+        let item = *reference;
+        let result = match item {
+            42 | 132 => "hit!",
+            _        => "miss"
+        };
+
+        if result == "hit!" {
+            println!("{}: {}", item, result);
+        }
+    }
+}
