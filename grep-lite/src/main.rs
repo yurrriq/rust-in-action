99a88874809069e480c4e6cfe6aa847dd3338a83
Eric Bailey <e.bailey@sportradar.com>
2018-11-29 23:26:57 -0600

    [ch02] Finish grep-lite

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -4,29 +4,50 @@ extern crate regex;
 
 use clap::{App,Arg};
 use regex::Regex;
+use std::fs::File;
+use std::io;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None    => (),
+        }
+    }
+}
 
 
 fn main() {
     let args = App::new("grep-lite")
-        .version("0.1.0")
-        .about("search for patterns")
+        .version("0.2.11")
+        .about("Search for patterns")
         .arg(Arg::with_name("pattern")
              .help("The pattern to search for")
              .takes_value(true)
              .required(true))
+        .arg(Arg::with_name("input")
+             .help("File to search")
+             .takes_value(true)
+             .required(false))
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
-
-    for line in quote.lines() {
-        match re.find(line) {
-            Some(_) => println!("{}", line),
-            None    => (),
+    match args.value_of("input") {
+        Some(input) => {
+            let file = File::open(input).unwrap();
+            let reader = BufReader::new(file);
+            process_lines(reader, re);
         }
+        None => {
+            let stdin  = io::stdin();
+            let reader = stdin.lock();
+            process_lines(reader, re)
+        },
     }
 }
