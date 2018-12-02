605195d9c9aa54a37c1085ef8c163b6e93b70d07
Eric Bailey <eric@ericb.me>
2018-12-01 23:30:40 -0600

    [ch03] Add working example of mock files

--- src/bin/ch3-mock-files.rs
+++ src/bin/ch3-mock-files.rs
@@ -1,6 +1,11 @@
 #![allow(unused_variables)]
 
-type File = String;
+#[derive(Debug)]
+
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
 
 fn open(f: &mut File) -> bool {
     true
@@ -12,15 +17,29 @@ fn close(f: &mut File) -> bool {
 
 #[allow(dead_code)]
 
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
-
-    unimplemented!()
-
+fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+    read_length
 }
 
 fn main() {
-    let mut f1 = File::from("f1.txt");
-    open(&mut f1);
-    // read(f1, vec![]);
-    close(&mut f1);
+    let mut f2 = File {
+        name: String::from("2.txt"),
+        data: vec![114, 117, 115, 116, 33],
+    };
+
+    let mut buffer: Vec<u8> = vec![];
+
+    open(&mut f2);
+    let f2_length = read(&f2, &mut buffer);
+    close(&mut f2);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f2);
+    println!("{} is {} bytes long", &f2.name, f2_length);
+    println!("{}", text)
 }
